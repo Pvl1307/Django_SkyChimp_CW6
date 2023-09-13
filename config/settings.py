6ffab200b2934037cdb8d2180ad0fea7d2ba1d2a
Pvl <ivanov.pavel.1307@gmail.com>
2023-09-13 14:45:38 +0200

    fix mailing settings

--- config/settings.py
+++ config/settings.py
@@ -134,12 +134,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 #  Рассылка почтой
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "pvl.ivnv@yandex.ru"
-EMAIL_HOST_PASSWORD = "Ppasha13"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
 
 
 
